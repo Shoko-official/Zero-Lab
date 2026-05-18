d50aff86ba338fa43b7ef45e287142f2901387e8
Shoko-official <a.breyton.pro@gmail.com>
2026-05-18 21:19:47 +0200

    feat: add AlphaZero evaluator adapters

--- src/zero_lab/search/alpha_zero/__init__.py
+++ src/zero_lab/search/alpha_zero/__init__.py
@@ -1,6 +1,7 @@
 """AlphaZero search implementation."""
 
 from zero_lab.search.alpha_zero.evaluator import AlphaZeroEvaluator, Evaluation
+from zero_lab.search.alpha_zero.model_evaluator import ModelEvaluator, UniformEvaluator
 from zero_lab.search.alpha_zero.search import AlphaZeroSearch, MCTSSearchConfig, SearchResult
 from zero_lab.search.alpha_zero.targets import select_action_by_temperature, visit_count_policy
 
@@ -9,7 +10,9 @@ __all__ = [
     "AlphaZeroSearch",
     "Evaluation",
     "MCTSSearchConfig",
+    "ModelEvaluator",
     "SearchResult",
+    "UniformEvaluator",
     "select_action_by_temperature",
     "visit_count_policy",
 ]
